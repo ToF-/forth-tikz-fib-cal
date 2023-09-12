39478ede69cc15e879ff64de2c1d355bf1e77299
ToF_ <cthibauttof@gmail.com>
2023-09-12 21:41:26 +0200

    advance and new direction wip

--- src/tikz-fib-cal.fs
+++ src/tikz-fib-cal.fs
@@ -16,8 +16,32 @@
  0  1 2constant dir-down
  1  0 2constant dir-right
 
+2variable direction
+
 : next-coord ( x,y,dx,dy -- x+dx,y+dy )
     rot + -rot + swap ;
 
+: advance ( x,y -- x',y' )
+    direction 2@ next-coord ;
+
 : next-direction ( d -- d' )
     dup 0= if swap negate swap then swap ;
+
+: next-direction! 
+    direction 2@ 
+    next-direction
+    direction 2! ;
+
+variable line-length
+
+: back ( n -- n*5-n )
+    dup dup line-length @ * - ;
+
+: new-line ( x,y -- x',y' )
+    direction 2@
+    dup 0<> if 
+        nip back 
+    else
+        drop back 
+        swap negate
+    then next-coord ;
--- test/tests.fs
+++ test/tests.fs
@@ -14,12 +14,19 @@ t{
     10 fib 55 ?s
 }t
 
-.( directions ) CR
+.( directions and next-coord ) CR
 t{
     10 6 dir-up    next-coord  5  ?s 10 ?s
     10 6 dir-left  next-coord  6  ?s  9 ?s
     10 6 dir-down  next-coord  7  ?s 10 ?s
     10 6 dir-right next-coord  6  ?s 11 ?s
+}t
+.( advance ) CR
+t{
+    dir-up direction 2! 
+    10 6 advance 5 ?s 10 ?s
+    dir-down direction 2!
+    10 6 advance 7 ?s 10 ?s
 }t
 .( next direction ) CR
 t{
@@ -27,5 +34,14 @@ t{
     dir-left  next-direction dir-down  ?d
     dir-down  next-direction dir-right ?d
     dir-right next-direction dir-up    ?d
+    dir-up direction 2! next-direction! direction 2@ dir-left ?d
+}t
+.( new line back ) CR
+t{
+    5 line-length !
+    0 0 dir-up    direction 2! new-line  4 ?s -1 ?s
+    0 0 dir-down  direction 2! new-line -4 ?s  1 ?s
+    0 0 dir-left  direction 2! new-line  1 ?s  4 ?s
+    0 0 dir-right direction 2! new-line -1 ?s -4 ?s
 }t
 bye
